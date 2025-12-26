a96be9355de472140a99760964bb34f5f3bdbe40
Doug Santos <douglasmartinssantos7@gmail.com>
2025-12-26 11:25:39 -0300

    video 6 part 2

--- lib/features/pokedex/screens/details/pages/widgets/detail_app_bar_widget.dart
+++ lib/features/pokedex/screens/details/pages/widgets/detail_app_bar_widget.dart
@@ -6,16 +6,32 @@ class DetailAppBarWidget extends StatelessWidget {
     super.key,
     required this.pokemon,
     required this.onBack,
+    required this.isOnTop,
   });
   final Pokemon pokemon;
   final VoidCallback onBack;
+  final bool isOnTop;
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
+      pinned: true,
       elevation: 0,
       backgroundColor: pokemon.baseColor,
       leading: IconButton(onPressed: onBack, icon: Icon(Icons.chevron_left)),
+      centerTitle: false,
+      title: AnimatedOpacity(
+        duration: Duration(milliseconds: 300),
+        opacity: isOnTop ? 0 : 1,
+        child: Text(
+          pokemon.name,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
